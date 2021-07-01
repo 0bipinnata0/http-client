a5d61728636baa0e511fd09f8a3c8d810a1f9437
夕向 <odinzero@outlook.com>
2021-07-01 10:14:00 +0800

    完成readrequestline状态机

--- requestParser.js
+++ requestParser.js
@@ -2,7 +2,21 @@ const Events = require("events");
 
 class RequestParser extends Events {
     _state = this._read_request_line
-    _message;
+    _message = {
+        request: {
+            method: '',
+            path: '',
+            version: '',
+            headers: [],
+            body: Buffer.from('')
+        },
+        response: {
+            status: 0,
+            headers: [],
+            body: Buffer.from('')
+        }
+    };
+    _cache;
 
     append(buffer) {
         for (let offset = 0; offset < buffer.length; offset++) {
@@ -10,12 +24,38 @@ class RequestParser extends Events {
         }
     }
 
+    /**
+     * buffer流参照ASCII
+     * Method SP Request-URI SP HTTP-Version CRLF
+     * SP 20
+     * CR 0D
+     * LF 0A
+     */
     _read_request_line(char) {
+        // [point,method,URI,version,CRLF]
+        let nextState = this._read_request_line
+        if (!this._cache) {
+            this._cache = [1, '', '', '', false]
+        }
+        if (char === 0x20) {
+            this._cache[0]++
+        } else if (char === 0x0D) {
+            this._cache[4] = true
+        } else if (char === 0x0A && this._cache[4]) {
+            this._message.request.method = this._cache[1];
+            this._message.request.path = this._cache[2];
+            this._message.request.version = this._cache[3];
 
+            // 状态切换前清空缓存
+            this._cache = null;
+            nextState = this._read_head_line
+        } else {
+            this._cache[this._cache[0]] += String.fromCharCode(char);
+        }
+        return nextState
     }
 
     _read_head_line(char) {
-
     }
 
     _read_body(char) {
